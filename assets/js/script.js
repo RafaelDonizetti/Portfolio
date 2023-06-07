e84b267302c1ab3704751dcb8dbc8abaf4f321a6
RafaelDonizetti <rafaeldonizetti20.13@gmail.com>
2023-06-07 20:15:00 -0300

    adicao de funcionalidade para o dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,26 @@
-window.addEventListener('load', function() {
-    const div = document.querySelector('.cotainer-principal')
-    div.style.opacity = 0;
+window.addEventListener("load", function () {
+  const div = document.querySelector(".cotainer-principal");
+  div.style.opacity = 0;
 
-    function fadeIn() {
-        let opacidade = 0
-        const temporizador = setInterval(function() {
-            if (opacidade >= 1) {
-                clearInterval(temporizador)
-            }
-            div.style.opacity = opacidade
-            opacidade += 0.1
-        }, 35)
-    }
-    fadeIn()
-})
+  function fadeIn() {
+    let opacidade = 0;
+    const temporizador = setInterval(function () {
+      if (opacidade >= 1) {
+        clearInterval(temporizador);
+      }
+      div.style.opacity = opacidade;
+      opacidade += 0.1;
+    }, 35);
+  }
+  fadeIn();
+});
+
+// Funcionamento do botão de navegação
+function toggleDropdown() {
+  var dropdown = document.getElementById("myDropdown");
+  if (dropdown.style.display === "none") {
+    dropdown.style.display = "block";
+  } else {
+    dropdown.style.display = "none";
+  }
+}
